f4f009a53e04f75371326fe49851c06069977bd5
antonio Silva <antonios@uoregon.edu>
2025-03-25 07:26:38 -0700

    leetcode

---
+++ Leetcode/python/sol3242.py
@@ -0,0 +1,34 @@
+class NeighborSum:
+
+    def __init__(self, grid: List[List[int]]):
+        self.size = len(grid)
+        self.grid = grid
+
+        # creates index with values' locations
+        self.index = {}
+        for i in range(self.size):
+            for j in range(self.size):
+                self.index[grid[i][j]] = (i,j)
+
+    def sum_neighbor(self, pos):  # helper function
+        suma = 0
+        for row, col in pos:
+            if (0 <= row < self.size) and (0 <= col < self.size):
+                suma+=self.grid[row][col]
+        return suma
+
+    def adjacentSum(self, value: int) -> int:
+        x, y = self.index[value] # value's loation in the grid
+        positions = [(x-1,y), (x+1,y), (x,y-1), (x,y+1)]
+        return self.sum_neighbor(positions)
+
+    def diagonalSum(self, value: int) -> int:
+        x, y = self.index[value] # value's loation in the grid
+        positions = [(x-1,y-1), (x-1,y+1),(x+1, y-1), (x+1,y+1)]
+        return self.sum_neighbor(positions)
+
+
+# Your NeighborSum object will be instantiated and called as such:
+# obj = NeighborSum(grid)
+# param_1 = obj.adjacentSum(value)
+# param_2 = obj.diagonalSum(value)
