cd7d019bdd72d89e7040f43f981a2cc549453a18
antonio Silva <antonios@uoregon.edu>
2025-04-22 12:03:14 -0700

    leetcode

--- Leetcode/python/sol1381.py
+++ Leetcode/python/sol1381.py
@@ -9,23 +9,23 @@ class CustomStack:
     def push(self, x: int) -> None:
         if self.size < self.maxSize:
             self.stack.append(x)
-            self.siz e+ =1
+            self.size+=1
 
     def pop(self) -> int:
         rslt = -1
         if self.size == 0:
             return rslt
-        self.siz e- =1
+        self.size-=1
         return self.stack.pop()
 
     def increment(self, k: int, val: int) -> None:
 
         if k > self.size:
             for i in range(0, self.size):
-                self.stack[i ]+ =val
+                self.stack[i]+=val
         else:
             for i in range(0, k):
-                self.stack[i ]+ =val
+                self.stack[i]+=val
 
 
             # Your CustomStack object will be instantiated and called as such:
