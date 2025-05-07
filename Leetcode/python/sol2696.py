01249bf6bd29283b3e37b7c40d586f6da0294c91
antonio Silva <antonios@uoregon.edu>
2025-05-07 09:41:37 -0700

    Leetcode

--- Leetcode/python/sol2696.py
+++ Leetcode/python/sol2696.py
@@ -2,8 +2,25 @@
 # https://leetcode.com/problems/minimum-string-length-after-removing-substrings/description/
 class Solution:
     def minLength(self, s: str) -> int:
-        while True:
-            length = len(s)
-            s = s.replace("AB", "").replace("CD", "")
-            if length == len(s):
-                return len(s)
+        stack = []
+        for i in s:
+            if stack:
+                if i == "B" and stack[-1] == "A":
+                    stack.pop()
+                elif i == "D" and stack[-1] == "C":
+                    stack.pop()
+                else:
+                    stack.append(i)
+            else:
+                stack.append(i)
+
+        return len(stack)
+        # while True:
+        #     length = len(s)
+        #     s = s.replace("AB", "").replace("CD", "")
+        #     if length == len(s):
+        #         return len(s)
+
+
+
+
