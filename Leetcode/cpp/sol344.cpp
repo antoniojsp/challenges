3ec5c348ea35b1469f43c7ec8712cb780465c98d
antonio Silva <antonios@uoregon.edu>
2025-05-11 02:39:09 -0700

    Leetcode

--- Leetcode/cpp/sol344.cpp
+++ Leetcode/cpp/sol344.cpp
@@ -1,6 +1,4 @@
-
 // https://leetcode.com/problems/reverse-string/description/
-
 
 class Solution {
 public:
@@ -14,6 +12,23 @@ public:
         int length = s.size();
         for(int i = 0; i<length/2;i++){
             swap(s[i], s[length-1-i]);
+        }
+    }
+};
+
+
+class Solution {
+public:
+    void swap(vector<char>& s, int i, int j){
+        s[i]=s[i]+s[j];
+        s[j]=s[i]-s[j];
+        s[i]=s[i]-s[j];
+    }
+
+    void reverseString(vector<char>& s) {
+        int length = s.size();
+        for(int i = 0; i<length/2;i++){
+            swap(s, i, length-1-i);
         }
     }
 };
--- Leetcode/curiosities/swap_xor.py
+++ Leetcode/curiosities/swap_xor.py
@@ -1,9 +1,16 @@
-a =19
-b = 11
-
+a =1
+b = 1
+print(a, b)
 a = a ^ b
-print(a)
 b = a ^ b
-print(b)
 a = a ^ b
-print(a)
+print(a, b)
+
+a = 2
+b = 4
+
+a = a + b # 6
+b = a - b # 2
+a = a - b
+print(a, b)
+
