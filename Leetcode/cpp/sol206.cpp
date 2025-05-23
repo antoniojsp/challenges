57eec857a0525b8c33078ce68138c475c20d7ab1
antonio Silva <antonios@uoregon.edu>
2025-05-22 17:38:25 -0700

    Leetcode

---
+++ Leetcode/cpp/sol206.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/reverse-linked-list/description/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* current = head;
+        ListNode* previous = nullptr;
+        while (current){
+            ListNode* next_node = current->next;
+            current->next = previous;
+            previous = current;
+            current = next_node;
+        }
+        return previous;
+    }
+};
