94e656aeccbefbe7f0161b79782b4f30a37151b2
antonio Silva <antonios@uoregon.edu>
2025-05-27 02:55:47 -0700

    Leetcode

---
+++ Leetcode/python/sol278.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/first-bad-version/description/
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 1
+        right = n
+        while left < right:
+            mid = left + (righ t -left )/ /2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
+
+
+
+
