aaa35e0fcf074145b12c5e515818cb116621110e
antonio Silva <antonios@uoregon.edu>
2025-05-29 02:32:28 -0700

    Leetcode

---
+++ Leetcode/python/sol20.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/valid-parentheses/description/
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+        stack = []
+        brackets = {']':'[', '}':'{', ')':'('}
+        for i in s:
+            if i in brackets:
+                if stack:
+                    if stack[-1] == brackets[i]:
+                        stack.pop()
+                    else:
+                        return False
+                else:
+                    return False
+            else:
+                stack.append(i)
+
+        return len(stack) == 0
+
