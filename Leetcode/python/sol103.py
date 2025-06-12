0bf5ee16e5a6072f907573e511c5940e27ddcba1
antonio Silva <antonios@uoregon.edu>
2025-06-12 04:39:39 -0700

    Leetcode

--- Leetcode/python/sol103.py
+++ Leetcode/python/sol103.py
@@ -30,3 +30,31 @@ class Solution:
 
         helper(root, 0)
         return [list(i) for i in res]
+
+
+
+
+from collections import defaultdict
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = defaultdict(list[int])
+        def helper(node:TreeNode, level:int):
+            if not node:
+                return
+            res[level].append(node.val)
+            helper(node.left, level+1)
+            helper(node.right, level+1)
+
+        helper(root, 0)
+        for i in range(len(res)):
+            if i%2 != 0:
+                res[i].reverse()
+
+        return list(res.values())
