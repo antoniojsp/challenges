f5a2c135a627254b078ec52ed879ba4620b5187a
antonio Silva <antonios@uoregon.edu>
2025-08-13 09:16:39 -0700

    Leetcode

--- Leetcode/python/sol2176.py
+++ Leetcode/python/sol2176.py
@@ -8,3 +8,20 @@ class Solution:
                 if nums[i] == nums[j] and i*j % k == 0:
                     count+=1
         return count
+
+
+class Solution:
+    def countPairs(self, nums: List[int], k: int) -> int:
+        indexes = defaultdict(list)
+        for idx, val in enumerate(nums):
+            indexes[val].append(idx)
+        count = 0
+
+        # values in each key represent equal values
+        # dict keeps the order
+        for arr_index in indexes.values():
+            for i in range(0, len(arr_index)):
+                for j in range(i+1, len(arr_index)):
+                    if (arr_index[i]*arr_index[j])%k==0:
+                        count += 1
+        return count
