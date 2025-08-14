c6629f74dcd8fab27667f8d0b743b71fd9885c7e
antonio Silva <antonios@uoregon.edu>
2025-08-14 00:41:31 -0700

    Leetcode

---
+++ Leetcode/python/sol3550.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/smallest-index-with-digit-sum-equal-to-index/description/
+class Solution:
+    def sum_digits(self, num):
+        rslt = 0
+        while 0 < num:
+            rsl t+ =nu m %10
+            nu m// =10
+        return rslt
+
+    def smallestIndex(self, nums: List[int]) -> int:
+        for i, val in enumerate(nums):
+            if i == self.sum_digits(val):
+                return i
+        return -1
+
+
