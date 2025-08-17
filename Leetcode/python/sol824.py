8fd18105580d90f940187c2c14ef28b5b8dad6ba
antonio Silva <antonios@uoregon.edu>
2025-08-17 01:44:32 -0700

    Leetcode

---
+++ Leetcode/python/sol824.py
@@ -0,0 +1,13 @@
+#  https://leetcode.com/problems/goat-latin/description/
+class Solution:
+    def toGoatLatin(self, sentence: str) -> str:
+        words = sentence.split(" ")
+        vowels = set("AEIOUaeiou")
+        for idx, word in enumerate(words):
+            if word[0] not in vowels: # check if first letter is a vowel or consonant
+                words[idx] = words[idx][1:] + word[0] # if consonant, remove first and add it to the end
+            words[idx ]+ ="ma" # all words get "ma" added
+            words[idx ]+ ="a " *(id x +1) # depending of the index, add "a" index number of times
+
+        return " ".join(words)
+
