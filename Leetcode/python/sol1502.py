9d7670e3b060015154e406edb4958e99e98b06c9
antonio Silva <antonios@uoregon.edu>
2025-08-17 10:24:32 -0700

    Leetcode

---
+++ Leetcode/python/sol1502.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/submissions/1738795008/
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr.sort()
+        diff = arr[1 ] -arr[0]
+        for i in range(1, len(arr)):
+            if arr[i ] -arr[ i -1] != diff:
+                return False
+        return True
+
