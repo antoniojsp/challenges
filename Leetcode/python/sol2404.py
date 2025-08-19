bace262c3313404c112be1eec09d0d59ffaeb70d
antonio Silva <antonios@uoregon.edu>
2025-08-18 20:25:53 -0700

    Leetcode

--- Leetcode/python/sol2404.py
+++ Leetcode/python/sol2404.py
@@ -7,3 +7,16 @@ class Solution:
         freq = list(Counter(even).items()) # count frequencies
         result = sorted(freq, key=lambda x: (-x[1], x[0])) # sorts
         return result[0][0] if result else -1
+
+
+class Solution:
+    def mostFrequentEven(self, nums: List[int]) -> int:
+        freq = list(Counter(i for i in nums if i%2==0).items()) # count frequencies
+        max_freq = -1
+        for i in freq:
+            max_freq = max(max_freq, i[1])
+        values_max_freq = [(i, j) for i, j in freq if j == max_freq]
+        min_val = float('inf')
+        for i in values_max_freq:
+            min_val = min(min_val, i[0])
+        return min_val if min_val != float('inf') else -1
