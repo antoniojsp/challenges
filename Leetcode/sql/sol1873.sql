10deca5fdc6729646d4051dfff5cdb3b41bc5d2a
antonio Silva <antonios@uoregon.edu>
2025-08-20 00:50:51 -0700

    Leetcode

---
+++ Leetcode/sql/sol1873.sql
@@ -0,0 +1,12 @@
+#https://leetcode.com/problems/calculate-special-bonus/
+
+# Write your MySQL query statement below
+SELECT
+  employee_id,
+  IF(
+    employee_id % 2 != 0 AND SUBSTRING(name, 1, 1) != 'M',
+    salary,
+    0
+  ) AS bonus
+FROM Employees
+ORDER BY employee_id;
