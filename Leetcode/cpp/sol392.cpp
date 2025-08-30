cca974a937273169f89c454da952d87e5505016b
antonio Silva <antonios@uoregon.edu>
2025-08-30 15:47:08 -0700

    Leetcode

---
+++ Leetcode/cpp/sol392.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/is-subsequence/description/
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if (s.size() == 0){
+            return true;
+        }
+
+        int s_idx = 0;
+        int t_idx = 0;
+        while (t_idx < t.size()){
+            if (s[s_idx] == t[t_idx]){
+                s_idx++;
+                if (s_idx == s.size()){
+                    return true;
+                }
+            }
+            t_idx++;
+        }
+
+        return false;
+    }
+};
