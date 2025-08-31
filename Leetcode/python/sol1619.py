58d53273d1ad5fa4700c6361a1eedf287b01ca72
Antonio <antoniojsp@gmail.com>
2025-08-31 00:21:01 -0700

    Sql

--- Leetcode/python/sol1619.py
+++ Leetcode/python/sol1619.py
@@ -7,3 +7,14 @@ class Solution:
 
         return sum(excluded) / len(excluded)
 
+
+class Solution:
+    def trimMean(self, arr: List[int]) -> float:
+        arr.sort()
+        sample = (len(arr) * 5) // 100  # number of elements to remove from the start and from the end
+        # excluded  = arr[sample:len(arr)-sample]
+        suma = 0
+        for i in range(sample, len(arr) - sample):
+            suma += arr[i]
+
+        return suma / (len(arr) - (2 * sample))
