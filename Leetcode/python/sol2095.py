db6f49a019676f41d1d5e707fdd814f578273c7e
Antonio <antoniojsp@gmail.com>
2025-09-05 08:58:52 -0700

    Sql

--- Leetcode/python/sol2095.py
+++ Leetcode/python/sol2095.py
@@ -1,6 +1,6 @@
 #https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
 
-# Definition for singly-linked list.
+## Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
@@ -19,7 +19,7 @@ class Solution:
             slow = slow.next
             fast = fast.next.next
 
-        if not prev:
+        if not prev: # if prev is None, then return None because the list only has one element that needs to be deleted
             return None
-        prev.next = prev.next.next
+        prev.next = prev.next.next # delete middle node
         return head
