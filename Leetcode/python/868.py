64d2ef9d077ca0cece6226d1186f1a5cf95c9290
Antonio <antoniojsp@gmail.com>
2025-09-07 20:48:35 -0700

    Sql

--- Leetcode/python/868.py
+++ Leetcode/python/868.py
@@ -11,3 +11,25 @@ class Solution:
                 max_dist = max(max_dist, i - last_seen)
                 last_seen = i
         return max_dist
+
+
+
+
+
+# bit manipulation
+
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        last_seen = None # set as none to detect the first '1'
+        max_dist = 0
+        pos = 0
+        while 0 < n:
+            if last_seen is None and n&1:
+                last_seen = pos
+            elif n&1:
+                max_dist = max(max_dist, pos - last_seen)
+                last_seen = pos
+            n>>=1
+            pos+=1
+
+        return max_dist
