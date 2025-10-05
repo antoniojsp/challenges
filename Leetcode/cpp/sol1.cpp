367d8d439ded5b246da1f2e9b117428e526df24a
Antonio <antoniojsp@gmail.com>
2025-10-04 21:25:39 -0700

    Sql

--- Leetcode/cpp/sol1.cpp
+++ Leetcode/cpp/sol1.cpp
@@ -16,3 +16,21 @@ public:
         return {};
     }
 };
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        /*Using a hashmap, check every value in "nums" and check
+        if you have seen the that what you need to reach the target sum
+        wtih your current value*/
+        unordered_map<int, int> seen; // hashmap
+        for (int i = 0;i<nums.size(); i++){
+            auto complement = target - nums[i]; // get how much you need to add to the current value to reach the target
+            if (seen.find(complement) != seen.end()){ // if you have already seen the complement, just return
+                return {i, seen[complement]};
+            }
+            seen[nums[i]] = i;
+        }
+        return {};
+    }
+};
