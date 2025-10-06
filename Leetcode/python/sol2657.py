4b926c52a35cec4acd7e2fc4ae24ac1641b2cc73
antonio Silva <antonios@uoregon.edu>
2025-10-06 05:42:01 -0700

    Macbook

---
+++ Leetcode/python/sol2657.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays/description/
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        """
+        Since values are distinct, then no need to check if the same index has the same value
+        """
+        seen = [0 ] *max( A +B)
+        rslt = []
+        count = 0
+        for i, j in zip(A, B):
+            seen[ i -1 ]+ =1
+            if seen[ i -1] == 2:
+                coun t+ =1
+            seen[ j -1 ]+ =1
+            if seen[ j -1] == 2:
+                coun t+ =1
+            rslt.append(count)
+        return rslt
+
