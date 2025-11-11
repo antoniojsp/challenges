b3dac7a827a5c7143bab48da8d9d5574bc8ab0f8
antonio Silva <antonios@uoregon.edu>
2025-11-11 11:28:07 -0800

    Macbook

--- Leetcode/python/sol1886.py
+++ Leetcode/python/sol1886.py
@@ -2,15 +2,19 @@
 class Solution:
     def rotate90(self, matrix):
         for i in range(len(matrix)):
-            for j in range( i +1, len(matrix[0])):
-                matrix[i][j], matrix[j][i] =  matrix[j][i], matrix[i][j]
+            for j in range(i + 1, len(matrix[0])):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         for i in matrix:
             i.reverse()
-        return matrix
 
     def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
-        for _ in range(4):
-            if self.rotate90(mat) == target:
+        if mat == target:
+            return True
+        for _ in range(3):
+            self.rotate90(mat)
+            if mat == target:
                 return True
+
         return False
+
 
