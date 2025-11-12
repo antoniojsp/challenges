a3b3b140b99d54d3134d2440c26d660f673503e2
antonio Silva <antonios@uoregon.edu>
2025-11-12 02:08:30 -0800

    Macbook

---
+++ Leetcode/python/sol2160.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/
+
+class Solution:
+    def minimumSum(self, num: int) -> int:
+        digits = sorted(list(str(num)))
+        return int(digits[0]+digits[2])+int(digits[1]+digits[3])
+
+
