2d735995b83ea919e74c83922490edc25086204c
antonio Silva <antonios@uoregon.edu>
2026-01-12 14:28:50 -0800

    Macbook

---
+++ Leetcode/python/sol1266.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/minimum-time-visiting-all-points/?envType=daily-question&envId=2026-01-12
+
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        distance = 0
+        for i in range(len(points)-1):
+            x1,x2 = points[i][0], points[i+1][0]
+            y1, y2 = points[i][1], points[i+1][1]
+            distance+=max(abs(x1-x2), abs(y1-y2))
+        return distance
--- Leetcode/python/sol2239.py
+++ Leetcode/python/sol2239.py
@@ -1,12 +1,13 @@
 # https://leetcode.com/problems/find-closest-number-to-zero/description/
 class Solution:
     def findClosestNumber(self, nums: List[int]) -> int:
-        gap = abs(nums[0])
+        gap = abs(nums[0])  # initial gap and result, first value of array
         result = nums[0]
         for i in nums[1:]:
-            if abs(i) < gap:
-                gap = abs(i)
+            distance = abs(i)
+            if distance < gap:  # if distance if less of currenet gap, update
+                gap = distance
                 result = i
-            elif abs(i) ==  gap:
+            elif distance == gap:  # if distance == gap, update only result with the max value
                 result = max(result, i)
         return result
