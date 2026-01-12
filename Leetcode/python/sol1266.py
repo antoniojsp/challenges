e6c294b2b3d20ece4452a864411152e51367acdd
antonio Silva <antonios@uoregon.edu>
2026-01-12 14:30:57 -0800

    Macbook

--- Leetcode/python/sol1266.py
+++ Leetcode/python/sol1266.py
@@ -4,7 +4,7 @@ class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
         distance = 0
         for i in range(len(points)-1):
-            x1,x2 = points[i][0], points[i+1][0]
-            y1, y2 = points[i][1], points[i+1][1]
+            x1,y1 = points[i]
+            x2, y2 = points[i+1]
             distance+=max(abs(x1-x2), abs(y1-y2))
         return distance
