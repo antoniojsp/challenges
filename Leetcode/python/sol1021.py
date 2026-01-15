76bff739af45ee583e27eef330838b455035aad4
antonio Silva <antonios@uoregon.edu>
2026-01-15 13:44:35 -0800

    Macbook

--- Leetcode/python/sol1021.py
+++ Leetcode/python/sol1021.py
@@ -1,2 +1,18 @@
 # https://leetcode.com/problems/remove-outermost-parentheses/
 
+
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        result = []
+        degree = 0
+        for i in s:
+            if i == "(":
+                if 0 < degree:
+                    result.append(i)
+                degree += 1
+            elif i == ")":
+                degree -= 1
+                if 0 < degree:
+                    result.append(i)
+        return "".join(result)
+
