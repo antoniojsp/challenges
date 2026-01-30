8bb9c549601de0695c48998c19824a94471b78fe
Antonio Silva Paucar <antoniojsp@gmail.com>
2026-01-30 14:17:10 -0800

    Windows

---
+++ Leetcode/typescript/sol226.ts
@@ -0,0 +1,40 @@
+
+// https://leetcode.com/problems/invert-binary-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+    if(!root){
+        return null;
+    }
+    const right = invertTree(root.right);
+    const left = invertTree(root.left);
+    root.right = left;
+    root.left = right;
+    return root;
+
+
+
+    // const S:TreeNode[] = [root]
+    // while(S.length > 0){
+    //     let node:TreeNode = S.pop()
+    //     if(node){
+    //         [node.left, node.right] = [node.right, node.left]
+    //         S.push(node.left)
+    //         S.push(node.right)
+    //     }
+    // }
+    // return root;
+};
