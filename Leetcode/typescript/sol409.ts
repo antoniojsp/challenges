d0e547075571ff8c9bf4a04a2133fbac004640a7
Antonio Silva Paucar <antoniojsp@gmail.com>
2026-01-31 22:18:35 -0800

    Windows

--- Leetcode/typescript/sol409.ts
+++ Leetcode/typescript/sol409.ts
@@ -1,7 +1,8 @@
 //https://leetcode.com/problems/longest-palindrome/
 
+
 const counter = (word:string):Map<string, number> => {
-    let count = new Map<string, number>();
+    let count:Map<string, number> = new Map<string, number>();
     for(const ch of word){
         count.set(ch, (count.get(ch)??0) + 1);
     }
@@ -9,16 +10,16 @@ const counter = (word:string):Map<string, number> => {
 }
 
 function longestPalindrome(s: string): number {
-    const ch_freq = counter(s);
-    let total = 0;
-    let num_of_odds = 0
+    const ch_freq:Map<string, number> = counter(s);
+    let total:number = 0;
+    let num_of_odds:number = 0;
     for(const [ch, freq] of ch_freq.entries()){
         if(freq % 2 == 0){
             total += freq;
         }else{
-            total+=(freq-1)
-            num_of_odds+=1
+            total+=(freq-1);
+            num_of_odds+=1;
         }
     }
-    return total + (num_of_odds > 0? 1:0)
+    return total + (num_of_odds > 0? 1:0);
 };
