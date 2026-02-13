6f0925e64aa5b68f11278fd9ed40f2c74cd70146
antonio Silva <antonios@uoregon.edu>
2026-02-13 15:08:12 -0800

    Macbook

--- Leetcode/python/sol133.py
+++ Leetcode/python/sol133.py
@@ -8,27 +8,53 @@ class Node:
 """
 from collections import deque
 from typing import Optional
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+from pprint import pprint
+from collections import deque
+from typing import Optional
+
+
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
-            return None
-        Q = deque([node])
-        seen = set()
-        clone_map = defaultdict(list)
-        # result = Node(val=)
+            return node
+        mapa = {}
+        mapa[node] = Node(val=node.val)
 
-        while Q:
-            node = Q.popleft()
-            if node in seen:
-                continue
-            if node:
-                # print(node.val)
-                seen.add(node)
-                for i in node.neighbors:
-                    clone_map[node.val].append(i.val)
-                    Q.append(i)
+        def helper(n):
+            for i in n.neighbors:
+                if i not in mapa:
+                    mapa[i] = Node(val=i.val)
+                    helper(i)
+                mapa[n].neighbors.append(mapa[i])
+
+        helper(node)
+        return mapa[node]
+        # if not node:
+        #     return None
+        # Q = deque([node])
+        # mapa = {}
+        # mapa[node] = Node(val=node.val)
+        # while Q:
+        #     curr = Q.popleft()
+        #     for i in curr.neighbors:
+        #         if i not in mapa:
+        #             mapa[i] = Node(val=i.val)
+        #             Q.append(i)
+        #         mapa[curr].neighbors.append(mapa[i])
+        # return mapa[node]
 
 
-        print(clone_map)
+
+
+
+
 
 
