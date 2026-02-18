21d9bfdee5fbd5c6125ffd826d230c9d48c1485f
Antonio Silva Paucar <antoniojsp@gmail.com>
2026-02-18 01:37:49 -0800

    Windows

--- Leetcode/typescript/sol693.ts
+++ Leetcode/typescript/sol693.ts
@@ -2,11 +2,19 @@
 // https://leetcode.com/problems/binary-number-with-alternating-bits/?envType=daily-question&envId=2026-02-18
 
 function hasAlternatingBits(n: number): boolean {
-    while(0 < n){
-        if(n%2 == (Math.trunc(n/2)%2)){
+    while (n > 0){
+        let curr = n&1;
+        let next = (n>>1)&1;
+        if(curr === next){
             return false;
         }
-        n = Math.trunc(n/2)
+        n = n >> 1;
     }
+    // while(0 < n){
+    //     if(n%2 == (Math.trunc(n/2)%2)){
+    //         return false;
+    //     }
+    //     n = Math.trunc(n/2)
+    // }
     return true;
 };
