45f8c76e750bfbdccaaa7ec3756af236eca0308e
antonio Silva <antonios@uoregon.edu>
2026-02-22 02:08:16 -0800

    Macbook

---
+++ Leetcode/typescript/sol868.ts
@@ -0,0 +1,29 @@
+
+
+// https://leetcode.com/problems/binary-gap/description/
+
+const decimalToBinary = (num: number):number[] =>{
+    const res:number[] = [];
+    while(0 < num){
+        res.push(num%2);
+        num=Math.trunc(num/2);
+    }
+    return res.reverse();
+}
+
+
+function binaryGap(n: number): number {
+    const binary:number[] = decimalToBinary(n);
+    let longest:number = 0;
+    let curr:number = 0;
+    for(const bit of binary){
+        console.log(bit, longest, curr)
+        if(bit == 1){
+            longest = Math.max(longest, curr);
+            curr = 1;
+        }else{
+            curr++;
+        }
+    }
+    return longest;
+};
