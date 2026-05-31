db33a1ec63d66ac791b8243630cb54f0588f9295
Antonio Silva Paucar <antoniojsp@gmail.com>
2026-05-31 03:28:58 -0700

    Dell

--- Leetcode/python/sol2126.py
+++ Leetcode/python/sol2126.py
@@ -16,3 +16,12 @@ class Solution:
 
         return True
 
+
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        asteroids.sort()
+        for i in asteroids:
+            if mass < i:
+                return False
+            mass+=i
+        return True
